071d4808bfb6096e155517f2d366b3dfbd1fccc8
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-19 16:59:59 +0100

    generazione password casuale

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php
+
+$passwordLength = $_GET['length'];
+
+if(isset($_GET['length'])) {
+    function generatePassword($passwordLength) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+        $characters_length = strlen($characters);
+        $password = '';
+
+        for($i = 0; $i < $passwordLength; $i++) {
+            $random = rand(0, $characters_length - 1);
+            $password .= $characters[$random];
+        }
+        return $password;
+
+
+    }
+    $generatedPassword = generatePassword($passwordLength);
+
+    
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -12,7 +37,15 @@
     <form action="index.php">
         <label for="length">Inserisci quanto vuoi che sia lunga la password</label>
         <input type="number" id="length" name="length" min="6" max="50">
-        <button type="sumbit">Genera Password</button>
+        <button type="submit">Genera Password</button>
     </form>
+
+    <?php
+
+    echo "<p>
+    La tua password è: $generatedPassword
+    ";
+
+    ?>
 </body>
 </html>
