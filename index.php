c2abe3d5b0c19781f97d88080adbc854a2f851b1
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-19 16:18:08 +0100

    inserimento in pagina del form

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>php-strong-password-generator</title>
 </head>
 <body>
-    prova
+    <h1>
+        Password Generator
+    </h1>
+    <form action="index.php">
+        <label for="length">Inserisci quanto vuoi che sia lunga la password</label>
+        <input type="number" id="length" name="length" min="6" max="50">
+        <button type="sumbit">Genera Password</button>
+    </form>
 </body>
 </html>
